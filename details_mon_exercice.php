82b3bd9fa37b945c08d9dafccaaed84cc9c043c1
JessyMuller <45139521+JessyMuller@users.noreply.github.com>
2018-11-19 16:23:47 +0100

    Add files via upload

---
+++ details_mon_exercice.php
@@ -0,0 +1,22 @@
+Bonjour, je vous joint ce petit document pour vous faire part de mon expérience à propos de cet exercice.
+
+J'ai commencé par intégrer toute la partie HTML et mis en place le css en même temps.
+
+Ensuite j'ai réalisé le Slider principale avec des images fictives trouvés sur google. 
+
+Je me suis occupé de la partie responsive à partir du moment ou je trouvais mon travail convenable.
+
+J'ai passé pas mal de temps sur certains détails pour que le resultat se rapproche le plus possible à votre demande.
+
+Pour terminer j'ai intégrer les dates avec PHP et j'y ai intégrer le slider en Javascript sur ces dates, le résultat n'est pas vraiment bon, j'ai reussi à afficher toutes les dates les unes à la suite des autres mais quand j'a voulu mettre en place le Javascript je n'avais plus que une une date d'affichée. Si l'on clique sur les flèches on peut voir que les dates slides bien mais malheureusement je n'ai pas reussi à en afficher plus d'une à la fois.
+
+
+Je tiens à préciser que ma formation au sein d'openclassrooms n'est pas officellement terminé, sur les 4 Projets à réaliser j'ai pu valider les deux premiers qui sont : Intégrer une maquette avec HTML/CSS et créer et mettre un site en valeur avec Wordpress. 
+Je devais réaliser les deux suivants qui sont sur les langages Javascript et PHP au cours d'un contrat de professionnalisation.
+
+Ce qui m'importe et d'acquérir de l'expérience un stage me convient très bien.
+
+
+Dans l'attente d'une réponse j'espère que mon travail vous conviendras.
+
+Cordialement Muller Jessy.
---
+++ index.php
@@ -0,0 +1,261 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <title>Titre</title>
+        <link rel="stylesheet" href="style.css" />
+        <link rel="stylesheet" media="screen and (max-width: 1160px)" href="responsiv.css" /> <!-- Pour ceux qui ont une résolution inférieure à 1160px et autre déclinaison -->
+    </head>
+
+    <body>
+    	<header>
+            <!--En-tête Menu de ma page-->
+
+    		<div id="menuTop">
+    		    <div id="logoLien">
+    			    <img src="assets/img/logo.png">
+    			    <div class="ligne_verticale" ></div>
+    			    <div id="lienCinema">
+    		            <a href="#">cinéma 1</a>
+    			        <a href="#">cinéma 2</a>
+    			        <a href="#">cinéma 3</a>
+    			    </div>
+    		    </div>
+                <div id="lienGeneral">
+    			
+    		
+    		        <div id="sectionPage">
+    		            <div class="programmeDL">
+    			            <a href="#">Programme PDF <img src="assets/img/download.png"></a>
+    			        </div>
+
+    			        <div id="page">
+    			            <a href="#">Nos Cinémas</a>
+    			            <div class="separation">-</div>
+    			            <a href="#">Scolaires</a>
+                			<div class="separation">-</div>
+                			<a href="#">Contact</a>
+                			<div class="separation">-</div>
+                			<a href="#">Infos pratiques</a>
+    		            </div>
+                		<div class="reseauxSociaux">
+                		    <img src="assets/img/facebook.jpg">
+                		    <div class="trait_verticale" ></div>
+                	        <img src="assets/img/twitter.jpg"><div class="bouton"><a href="#">BILLETERIE</a><div class="hover">
+                                <ul class="listeHover">
+                                    <li><a href="#">Cinema 1</a></li>
+                                    <li><a href="#">Cinema 2</a></li>
+                                    <li><a href="#">Cinema 3</a></li>
+                                </ul>
+                            </div></div>
+                		</div>
+                		    
+            		</div>
+
+    			    <div id="event">
+    			     	<div class="element"><a href="#">PROGRAMMATION</a>
+                            <div class="hover">
+                                <ul class="listeHover">
+                                    <li><div class="puce"></div><a href="#">Cinema 1</a></li>
+                                    <li><div class="puce"></div><a href="#">Cinema 2</a></li>
+                                    <li><div class="puce"></div><a href="#">Cinema 3</a></li>
+                                    <li><div class="puce"></div><a href="#">Calendrier</a></li>
+                                </ul>
+                            </div>
+    					</div>
+        				<div class="separation">-</div>
+        				<div class="element"><a href="#">EVENEMENT</a>
+                        <div class="hover">
+                                <ul class="listeHover">
+                                    <li><div class="puce"></div><a href="#">Evenement 1</a></li>
+                                    <li><div class="puce"></div><a href="#">Evenement 2</a></li>
+                                    <li><div class="puce"></div><a href="#">Evenement 3</a></li>
+                                    <li><div class="puce"></div><a href="#">Calendrier</a></li>
+                                </ul>
+                            </div>
+                        </div>
+        				<div class="separation">-</div>
+        				<div class="element"><a href="#">AVANT-PREMIERE</a>
+                        <div class="hover">
+                                <ul class="listeHover">
+                                    <li><div class="puce"></div><a href="#">Avant-première 1</a></li>
+                                    <li><div class="puce"></div><a href="#">Avant-première  2</a></li>
+                                    <li><div class="puce"></div><a href="#">Avant-première 3</a></li>
+                                    <li><div class="puce"></div><a href="#">Calendrier</a></li>
+                                </ul>
+                            </div>
+                        </div>
+        				<div class="separation">-</div>
+        				<div class="element"><a href="#">RENDEZ-VOUS</a>
+                        <div class="hover">
+                                <ul class="listeHover">
+                                    <li><div class="puce"></div><a href="#">Rendez-vous 1</a></li>
+                                    <li><div class="puce"></div><a href="#">Rendez-vous 2</a></li>
+                                    <li><div class="puce"></div><a href="#">Rendez-vous 3</a></li>
+                                    <li><div class="puce"></div><a href="#">Calendrier</a></li>
+                                </ul>
+                            </div>
+                        </div>
+        				<div class="separation">-</div>
+        				<div class="element"><a href="#">SORTIE NATIONALE</a>
+                        <div class="hover">
+                                <ul class="listeHover">
+                                    <li><div class="puce"></div><a href="#">Sortie nationale 1</a></li>
+                                    <li><div class="puce"></div><a href="#">Sortie nationale 2</a></li>
+                                    <li><div class="puce"></div><a href="#">Sortie nationale 3</a></li>
+                                    <li><div class="puce"></div><a href="#">Calendrier</a></li>
+                                </ul>
+                            </div>
+                        </div>
+        				<div class="separation">-</div>
+        				<div class="element"><a href="#">PUBLIC</a><div class="hover">
+                                <ul class="listeHover">
+                                    <li><div class="puce"></div><a href="#">Public 1</a></li>
+                                    <li><div class="puce"></div><a href="#">Public 2</a></li>
+                                    <li><div class="puce"></div><a href="#">Public 3</a></li>
+                                    <li><div class="puce"></div><a href="#">Calendrier</a></li>
+                                </ul>
+                            </div>
+                        </div>
+    			    </div>
+                </div>
+    		</div>
+    	</header>
+
+    	<section id="corps">
+
+            <div class="contenuSlider">
+
+                <div class="mySlides anim">
+
+                    <div class="slide">
+                        <div class="imgSlide">
+                            <img src="assets/img/bck.png">
+                        </div>
+                        <div class="txtSlide">
+                            <h2>SORTIE <br> NATIONALE</h2>
+                            <p><span>Don't Worry, <br>He Won't Get Far On Foot</span><br><br> de Gus Van Sant <br>2018</p>
+
+
+                        </div>
+                    </div>
+                </div>
+
+                <div class="mySlides anim">
+                    <div class="slide">
+                        <div class="imgSlide">
+                            
+                            <img src="assets/img/bg1.jpg">
+                        </div>
+                        <div class="txtSlide">
+                            <h2>FILM <br> ENFANT</h2>
+                            <p><span>Notre séléction, <br>Retrouvez nos films pour enfants</span></p>
+
+
+                        </div>
+                    </div>
+                </div>
+
+                <div class="mySlides anim">
+                    <div class="slide">
+                        <div class="imgSlide">
+                            <img src="assets/img/bg2.jpg">
+                        </div>
+                        <div class="txtSlide">
+                            <h2>NOUVEAU <br> FILMS</h2>
+                            <p><span>DERNIER FILM <br>Voici nos dernier film tous public</span><br><br> Sortie Octobre <br>2018</p>
+
+                        </div>
+                    </div>
+                </div>
+
+                <div id="rond">
+                    <span class="dot"></span> 
+                    <span class="dot"></span> 
+                    <span class="dot"></span> 
+                </div>
+            </div>
+        </section>
+
+        <section>
+            <div id="programmation_section">
+                <h2>Programmatio</h2>
+                <div class="tableau">
+                    <a class="prec" onclick="plusSlides(-1)">&#10094;</a>
+                    <div class="ligne_verticale" ></div>
+                        <?php
+
+                            $programmationDates = [
+                              '2018-10-10',
+                              '2018-10-12',
+                              '2018-10-13',
+                              '2018-10-14',
+                              '2018-10-18',
+                              '2018-10-20',
+                              '2018-10-22',
+                              '2018-10-23',
+                            ];
+
+                            foreach($programmationDates as $valeur){
+                                echo "<div class='date fade'>$valeur</div>";
+                            }
+                        ?>
+
+                    <div class="ligne_verticale" ></div>
+                        <a class="suiv" onclick="plusSlides(1)">&#10095;</a>
+
+                    <div id="calendrierSemaine"><a href="#">Calendrier par semaine<img src="assets/img/calendar.png"></a></div>
+
+
+                    <script>
+                        var Index = 0;
+                    showDates(Index);
+
+                    // Next/previous controls
+                    function plusSlides(n) {
+                      showDates(Index += n);
+                    }
+
+                    // Thumbnail image controls
+                    function currentSlide(n) {
+                      showDates(Index = n);
+                    }
+
+                    function showDates(n) {
+                      var i;
+                      var dates = document.getElementsByClassName("date");
+                      if (n > dates.length) {Index = 1}
+                      if (n < 1) {Index = dates.length}
+                      for (i = 0; i < dates.length; i++) {
+                          dates[i].style.display = "none";
+                      }
+                      dates[Index-1].style.display = "block";
+                    } 
+                    </script>
+                </div>
+            </div>
+        </section>
+
+        <script>
+            var slideIndex = 0;
+            showSlides();
+
+            function showSlides() {
+                var i;
+                var slides = document.getElementsByClassName("mySlides");
+                var dots = document.getElementsByClassName("dot");
+                for (i = 0; i < slides.length; i++) {
+                   slides[i].style.display = "none";  
+                }
+                slideIndex++;
+                if (slideIndex > slides.length) {slideIndex = 1}    
+                for (i = 0; i < dots.length; i++) {
+                    dots[i].className = dots[i].className.replace(" active", "");
+                }
+                slides[slideIndex-1].style.display = "block";  
+                dots[slideIndex-1].className += " active";
+                setTimeout(showSlides, 2000); // Change image every 2 seconds
+            }
+        </script>
+    </body>
+</html>
